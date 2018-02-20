2ba2205c565db0d06866c7b7694853ca81015d52
Brandon Covington <brandoncovington@Interapts-MacBook-Pro.local>
2018-02-20 15:13:43 -0500

    assessment4

--- assessment4/main.swift
+++ assessment4/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+//Arrays
+//1.
+//a. Create an array of integers named intArray that contains all the integers from 0 to 10, and print the array after.
+//b. Remove the element at index 0 from the array, and print out the array after.
+//c. Append new elements to the array, the integers 11 and 12, and print out the array after.
+//d. Print out the number of items in the intArray.
+var IntArray = [0...10]
+
+
+//Functions
+//2.
+//a. Create a function called multiply that takes two integers as input parameters, and returns the result of multiplying the two numbers.
+func multipy(num1: Int, num2: Int) -> Int {
+    return num1 * num2
+   
+}
+//b. Call the function with the input numbers 2 and 3, and print out the result.
+func addEverythingtogether(IntArray: [Int]) -> String {
+    var completeint = ""
+    let number = IntArray
+    print(addEverythingtogether(IntArray: IntArray))
+    return completeint
+    
+}
+
+
+//3.
+//a. Create another function called checkEven that takes in an integer as input, that returns a boolean value true if the number is even or false if the number is odd.
+func checkEven()
+//b. Call the checkEven function and use a print statement to print "It's even" if the function returns true or "It's odd" if the function returns false.
+
+//Bonus
+//4. Look up the dictionary collection type and create a dictionary called stateDictionary containing five abbreviations and their state names, with the abbreviations as the key and the state name as the value.
 
